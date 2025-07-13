7af3426e9f4b4539e5ae44757fc5decd95973226
wywang27 <wywang27@gmail.com>
2025-07-13 04:54:10 +0000

    Deploying to gh-pages from @ wywang27/wywang27.github.io@d7b74077b64731f9670f671a3f6f4b8477560cbb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-blogs",
+          title: "Blogs",
           description: "",
           section: "Navigation",
           handler: () => {

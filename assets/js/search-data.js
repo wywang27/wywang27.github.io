3aef4d5c89bbff6d6d4b72fd680ba6dc6c97464b
wywang27 <wywang27@gmail.com>
2025-07-14 03:56:06 +0000

    Deploying to gh-pages from @ wywang27/wywang27.github.io@3af4b24ae4e1cabc4fea16fcb2752e412bc4f813 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-paddling-san-marcos",
+        
+          title: "Paddling San Marcos",
+        
+        description: "Having fun in San Marcos",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/San-Marcos/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -251,17 +262,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2023/audios/";
-          
-        },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
           
         },
       },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
